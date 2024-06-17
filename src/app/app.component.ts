c999339ab3f4ac0af66c92eecf449ef9172322d6
Leandro-Caponetto <caponettopeppers@gmail.com>
2024-06-17 16:42:27 -0300

    Upload-Files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MediaService } from './media.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'file_client';
+  url?: string;
+  constructor(
+    private mediaService: MediaService
+  ){}
+
+  upload(event: any){
+    const file = event.target.files[0];
+    if (file){
+      const formData = new FormData;
+      formData.append('file', file);
+
+      this.mediaService.uploadFile(formData)
+      .subscribe(reponse => {
+        this.url = reponse.url;
+      })
+    }
+  }
 }
